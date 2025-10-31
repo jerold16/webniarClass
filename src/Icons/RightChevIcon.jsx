a524d0719766ec855504476e27c6563dee5d5e5b
Jerold <jeroldraja12@gmail.com>
2025-10-31 12:18:22 +0530

    last push

---
+++ src/Icons/RightChevIcon.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const RightChevIcon = ({ size }) => {
+    return (
+        <svg xmlns="http://www.w3.org/2000/svg" width={size || "16"} height={size || "16"} fill="currentColor" className="bi bi-chevron-right" viewBox="0 0 16 16">
+            <path strokeWidth={1.4} stroke='currentColor'
+                fillRule="evenodd" d="M4.646 1.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1 0 .708l-6 6a.5.5 0 0 1-.708-.708L10.293 8 4.646 2.354a.5.5 0 0 1 0-.708" />
+        </svg>
+    )
+}
+
+export default RightChevIcon
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,52 @@
 import React from 'react'
+import RightChevIcon from '../Icons/RightChevIcon'
+import FacebookIcon from '../Icons/FacebookIcon'
 
 const Footer = () => {
     return (
-        <div>
-            Footer
+        <div className=' min-h-[40vh] bg-[#222222] text-white '>
+            <main className=' row py-3 container mx-auto ' >
+                <section className=' col-md-6 ' >
+                    <div className=' flex items-center gap-2 ' >
+                        <RightChevIcon />
+                        <div className=' text-xl fw-medium text-[#8cad28] ' >
+                            Let's Talk
+                        </div>
+                    </div>
+                    <div>
+                        <h6 className=' text-3xl! lg:text-5xl! my-2 ' > We're just a click away! </h6>
+                        <p className=' text-slate-400 text-lg ' >
+                            Need to know more about our services? Feel free to contact us anytime. We’re here to help you build your dream space!
+                        </p>
+                    </div>
+                    {/* Numbers */}
+                    <div className=' text-slate-400 flex gap-3 items-center ' >
+                        <a href="tel:+919797979797" target='_blank' className=' text-decoration-none text-white text-2xl ' > +919797979797 </a>
+                        or
+                        <a href="https://wa.me/+919797979797" target='_blank' className=' text-decoration-none text-white text-2xl '  > Chat Now </a>
+                    </div>
+                    {/* Social media links */}
+                    <div className=' flex items-center gap-2 ' >
+                        <div className=' rounded-full! p-1 bg-[#2b2b2b] text-white  cursor-pointer 
+                        w-[3rem] flex items-center justify-content-center h-[3rem] pb-0 ' >
+                            <a href='https://facebook.com' target='_blank' className=' text-white! ' >
+                                <FacebookIcon />
+                            </a>
+                        </div>
+                    </div>
+                </section>
+                <section className=' col-md-3 ' >
+                    <h6 className=' text-3xl! ' >Office Address</h6>
+                    <p className=' text-slate-400 ' >
+                        9A, Tamilnadu Grama Bank 1st Floor,
+                        Trichy -Karur Main Road,
+                        Kadambar Kovil,
+                        Kulithalai- 639104
+                    </p>
+                </section>
+
+            </main>
+
         </div>
     )
 }
